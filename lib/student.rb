6213e3dec7464169d2327b52dad06db4764049ca
Matthew Williams <matthew.williams@flatironschool.com>
2021-05-22 22:17:32 -0600

    commit tree eee8a37d3a47aefd8b055de120b22dcece7db444

--- lib/student.rb
+++ lib/student.rb
@@ -1,7 +1,19 @@
 require_relative "../config/environment.rb"
 require 'active_support/inflector'
 require 'interactive_record.rb'
+require 'pry'
 
 class Student < InteractiveRecord
+
+    self.column_names.each do |col_name|
+        attr_accessor col_name.to_sym
+      end
+
+    def initialize(attributes={})
+
+        attributes.each do |property, value|
+            self.send("#{property}=", value)
+          end
+    end 
 
 end
