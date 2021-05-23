d3629b5b1cc1a9a60f069543fb25af3b244bc201
Matthew Williams <matthew.williams@flatironschool.com>
2021-05-22 22:17:39 -0600

    Done.

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,5 +2,56 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+
+    def self.table_name
+        self.to_s.downcase.pluralize
+    end 
+
+    def self.column_names
+
+        column_names = []
+
+        sql = "PRAGMA table_info(#{self.table_name})"
+        table_info = DB[:conn].execute(sql)
+
+        table_info.each do |column|
+            column_names << column["name"]
+        end 
+
+        column_names
+    end 
   
+    def table_name_for_insert
+        self.class.table_name
+    end 
+
+    def col_names_for_insert
+        self.class.column_names.filter{|str| str != "id"}.join(", ")
+    end 
+
+    def values_for_insert
+        values = []
+ 
+        self.class.column_names.each do |attr|
+        values << "'#{send(attr)}'" unless send(attr).nil?
+        end
+        values.join(", ")
+    end 
+
+    def save
+        sql = "INSERT INTO #{table_name_for_insert} (#{col_names_for_insert}) VALUES (#{values_for_insert})"
+        DB[:conn].execute(sql)
+        @id = DB[:conn].execute("SELECT last_insert_rowid() FROM #{table_name_for_insert}")[0][0]    
+    end 
+
+    def self.find_by_name(name)
+        sql = "SELECT * FROM #{self.table_name} WHERE name = ?"
+        DB[:conn].execute(sql, name)
+         
+    end 
+
+    def self.find_by(atr)
+        sql = "SELECT * FROM #{self.table_name} WHERE #{atr.keys.first.to_s} = ?"
+        DB[:conn].execute(sql, atr.values.first)
+    end 
 end
